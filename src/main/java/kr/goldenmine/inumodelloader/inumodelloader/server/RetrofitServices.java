2c2810bb15d73495bed9e931a64ff595bd73cae0
GoldenMine0502 <ehe123@naver.com>
2023-04-07 15:49:13 +0900

    코드 내 retrofit 제거

--- src/main/java/kr/goldenmine/inumodelloader/inumodelloader/server/RetrofitServices.java
+++ src/main/java/kr/goldenmine/inumodelloader/inumodelloader/server/RetrofitServices.java
@@ -1,14 +1,14 @@
 package kr.goldenmine.inumodelloader.inumodelloader.server;
 
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+//import retrofit2.Retrofit;
+//import retrofit2.converter.gson.GsonConverterFactory;
+//import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitServices {
-    public static final StatisticsService STATISTICS_SERVICE = new Retrofit.Builder()
-            .baseUrl("https://localhost:20200/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-            .create(StatisticsService.class);
+//    public static final StatisticsService STATISTICS_SERVICE = new Retrofit.Builder()
+//            .baseUrl("https://localhost:20200/")
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .addConverterFactory(ScalarsConverterFactory.create())
+//            .build()
+//            .create(StatisticsService.class);
 }
--- src/main/java/kr/goldenmine/inumodelloader/inumodelloader/server/ServerEvents.java
+++ src/main/java/kr/goldenmine/inumodelloader/inumodelloader/server/ServerEvents.java
@@ -3,13 +3,13 @@ package kr.goldenmine.inumodelloader.inumodelloader.server;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import okhttp3.ResponseBody;
+//import okhttp3.ResponseBody;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+//import retrofit2.Call;
+//import retrofit2.Callback;
+//import retrofit2.Response;
 
 public class ServerEvents {
     // Directly reference a log4j logger.
@@ -40,53 +40,53 @@ public class ServerEvents {
     @SubscribeEvent
     public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
         // 플레이어 접속 확인
-        if(event.getPlayer() instanceof ServerPlayerEntity) {
-            try {
-                RetrofitServices.STATISTICS_SERVICE.joinRequest(event.getPlayer().getName().getString()).enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(@NotNull Call<ResponseBody> call, @NotNull Response<ResponseBody> response) {
-                        if (response.isSuccessful()) {
-                            LOGGER.info("(join) sended to server sucessfully");
-                        } else {
-                            LOGGER.info("(join) connected but not succeed");
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(@NotNull Call<ResponseBody> call, @NotNull Throwable t) {
-                        LOGGER.error(t.getMessage(), t);
-                    }
-                });
-            } catch(Exception ex) {
-                ex.printStackTrace();
-                LOGGER.info("single play joined");
-            }
-        }
+//        if(event.getPlayer() instanceof ServerPlayerEntity) {
+//            try {
+//                RetrofitServices.STATISTICS_SERVICE.joinRequest(event.getPlayer().getName().getString()).enqueue(new Callback<ResponseBody>() {
+//                    @Override
+//                    public void onResponse(@NotNull Call<ResponseBody> call, @NotNull Response<ResponseBody> response) {
+//                        if (response.isSuccessful()) {
+//                            LOGGER.info("(join) sended to server sucessfully");
+//                        } else {
+//                            LOGGER.info("(join) connected but not succeed");
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(@NotNull Call<ResponseBody> call, @NotNull Throwable t) {
+//                        LOGGER.error(t.getMessage(), t);
+//                    }
+//                });
+//            } catch(Exception ex) {
+//                ex.printStackTrace();
+//                LOGGER.info("single play joined");
+//            }
+//        }
     }
 
     @SubscribeEvent
     public void onPlayerQuit(PlayerEvent.PlayerLoggedOutEvent event) {
-        if(event.getPlayer() instanceof ServerPlayerEntity) {
-            try {
-                RetrofitServices.STATISTICS_SERVICE.quitRequest(event.getPlayer().getName().getString()).enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(@NotNull Call<ResponseBody> call, @NotNull Response<ResponseBody> response) {
-                        if (response.isSuccessful()) {
-                            LOGGER.info("(quit) sended to server sucessfully");
-                        } else {
-                            LOGGER.info("(quit) connected but not succeed");
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(@NotNull Call<ResponseBody> call, @NotNull Throwable t) {
-                        LOGGER.error(t.getMessage(), t);
-                    }
-                });
-            } catch(Exception ex) {
-                ex.printStackTrace();
-                LOGGER.info("multi play joined");
-            }
-        }
+//        if(event.getPlayer() instanceof ServerPlayerEntity) {
+//            try {
+//                RetrofitServices.STATISTICS_SERVICE.quitRequest(event.getPlayer().getName().getString()).enqueue(new Callback<ResponseBody>() {
+//                    @Override
+//                    public void onResponse(@NotNull Call<ResponseBody> call, @NotNull Response<ResponseBody> response) {
+//                        if (response.isSuccessful()) {
+//                            LOGGER.info("(quit) sended to server sucessfully");
+//                        } else {
+//                            LOGGER.info("(quit) connected but not succeed");
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(@NotNull Call<ResponseBody> call, @NotNull Throwable t) {
+//                        LOGGER.error(t.getMessage(), t);
+//                    }
+//                });
+//            } catch(Exception ex) {
+//                ex.printStackTrace();
+//                LOGGER.info("multi play joined");
+//            }
+//        }
     }
 }
