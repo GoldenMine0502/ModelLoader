55e310fa2a4f883d95694fa4ccef755103de1779
GoldenMine0502 <ehe123@naver.com>
2023-11-23 02:04:56 +0900

    retrofit 활성화, asset 종류 일부 git에서 제거

--- src/main/java/kr/goldenmine/inumodelloader/inumodelloader/server/StatisticsService.java
+++ src/main/java/kr/goldenmine/inumodelloader/inumodelloader/server/StatisticsService.java
@@ -1,13 +1,13 @@
 package kr.goldenmine.inumodelloader.inumodelloader.server;
 
-//import okhttp3.ResponseBody;
-//import retrofit2.Call;
-//import retrofit2.http.POST;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.POST;
 
 public interface StatisticsService {
-//    @POST("/statistics/join")
-//    Call<ResponseBody> joinRequest(String username);
-//
-//    @POST("/statistics/quit")
-//    Call<ResponseBody> quitRequest(String username);
+    @POST("/statistics/join")
+    Call<ResponseBody> joinRequest(String username);
+
+    @POST("/statistics/quit")
+    Call<ResponseBody> quitRequest(String username);
 }
